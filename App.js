6fc809144a380b5909b58f2a9342c2c312a05d4b
Rubén Martínez <rumama02@floridauniversitaria.es>
2024-10-16 14:33:27 +0200

    Ejercicio 8

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
 /** IMPORTEM les llibreries necessàries */
 import React from 'react';
-import {ScrollView, StatusBar, StyleSheet, View,} from 'react-native';
+import {StyleSheet} from 'react-native';
 import { PaperProvider, Text, TextInput } from 'react-native-paper';
-const info = ["Email","Nom"];
-const estil = 'florida';
+
+const info = ["Email", "Nom"];
+const estil = 'upv'; // Canvia a 'upv' segons vulguis
+
 const nom = (textAMostrar, estilo) => {
   return (
     <Text style={estilo}>{textAMostrar}</Text>
   );
 };
 
-const dades = (prueba) => {
+const dades = (prueba, colorText, placeholderColor) => {
   return (
     prueba.map((texto, index) => (
       <TextInput
         key={index} // Añadir una key única para cada elemento
         label={texto}
         value={texto}
+        style={{ color: colorText }} // Aplicar color al text
+        placeholderTextColor={placeholderColor} // Aplicar color al placeholder
       />
     ))
   );
@@ -24,41 +28,25 @@ const dades = (prueba) => {
 
 /** A la funció App, dins del return crearem la nostra pantalla */
 const App = () => {
-
+  // Determinem l'estil a aplicar i els colors dinàmics en funció de la constant 'estil'
   const estilSeleccionat = estil === 'florida' ? estils.florida : estils.upv;
+  const colorText = estil === 'florida' ? 'white' : 'orange';
+  const placeholderColor = estil === 'florida' ? 'white' : 'orange';
 
   return (
     <PaperProvider>
       {nom("Rubén Martínez Martínez", estilSeleccionat)}
-      {dades(info)}
+      {dades(info, colorText, placeholderColor)}
     </PaperProvider>
   );
 };
 
 /** En aquesta secció, crearem els estils a aplicar als nostres components */
 const estils = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
   titol: {
     fontSize: 32,
     fontWeight: 'bold',
   },
-  descripcio: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  upv:{
-    backgroundColor: 'purple',
-    fontSize: 10,
-    fontWeight: '600',
-    padding: 4,
-    paddingLeft: 12,
-    textAlign: 'left',
-    color: 'grey',
-    },
   florida: {
     backgroundColor: 'red',
     fontSize: 12,
@@ -66,7 +54,15 @@ const estils = StyleSheet.create({
     padding: 4,
     paddingRight: 12,
     textAlign: 'right',
-    },
+  },
+  upv: {
+    backgroundColor: 'purple',
+    fontSize: 10,
+    fontWeight: '600',
+    padding: 4,
+    paddingLeft: 12,
+    textAlign: 'left',
+  },
 });
 
 export default App;
